574d45d38cc83da4367e50fcd0603d3ea80a13dc
Vasilis CodeX <vasilis.tris@gmail.com>
2020-09-20 21:34:16 +0300

    Adding code on routers and deleting old forecast & geolocation utils

--- src/routers/forecast.router.js
+++ src/routers/forecast.router.js
@@ -1,6 +1,31 @@
 const express = require("express");
+const request = require("request");
+
 const router = new express.Router();
 
-router.get('/forecast', (request, response) => {});
+router.get('/forecast', (req, res) => {
+    let longitude = req.body.longitude;
+    let latitude = req.body.latitude;
+
+    const darkskyUrl = "https://api.darksky.net/forecast/" + process.env.DARKSKY_API_TOKEN + "/" + longitude + "," + latitude + "?units=si";
+	request({url: darkskyUrl, json: true}, (error, {body, statusCode}) => {
+		if(error) {
+			callback(`Could not connect with the weather forecast dark-sky API service!`, undefined);
+		} else if(statusCode != 200) {
+			callback(`Error occurred, error code: ${statusCode} Error: ${error}`);
+		} else if(typeof body.currently === "undefined") {
+			callback(`Error on weather forecast data!`);
+		} else {
+			const data = body;
+			callback(undefined, {
+				temperature: data.currently.temperature,
+				rainPerc: data.currently.precipProbability
+			});
+		}
+	});
+});
+
+
+
 
 module.exports = router;
--- src/routers/geolocation.router.js
+++ src/routers/geolocation.router.js
@@ -1,6 +1,28 @@
 const express = require("express");
+const request = require("request");
+
 const router = new express.Router();
 
-router.get('/geolocation', (request, response) => {});
+router.get('/geolocation', (req, res) => {
+    const address = req.body.address;
+    const mapboxUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + address + ".json?access_token=" + process.env.MAPBOX_API_TOKEN;
+	
+	request({url: mapboxUrl, json: true}, (error, {body, statusCode}) => {
+		if(error) {
+			callback("Unable to connect to service!", undefined);
+		} else if(body.features.length === 0) {
+			callback("Unable to find location! Try another location...", undefined);
+		} else if(statusCode != 200) {
+			callback("Something went wrong! " + error, undefined);
+		} else {
+			const data = body;
+			callback(undefined, {
+				longitude: data.features[0].center[1],
+				latitude: data.features[0].center[0],
+				location: data.features[0].place_name
+			});
+		}
+	});	
+});
 
 module.exports = router;
