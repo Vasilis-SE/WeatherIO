3a858e9c697d3a1d7e55db98a6f52d7c000601f0
Vasilis CodeX <vasilis.tris@gmail.com>
2020-09-21 18:40:33 +0300

    Migrating to superAgent HTTP call module

--- src/routers/forecast.router.js
+++ src/routers/forecast.router.js
@@ -1,28 +1,21 @@
 const express = require("express");
-const request = require("request");
+const superagent = require('superagent');
 
 const router = new express.Router();
 
-router.get('/forecast', (req, res) => {
-    let longitude = req.body.longitude;
-    let latitude = req.body.latitude;
+router.get('/forecast/:lon/:lat', (req, res) => {
+    let longitude = req.params.lon;
+    let latitude = req.params.lat;
 
-    const darkskyUrl = "https://api.darksky.net/forecast/" + process.env.DARKSKY_API_TOKEN + "/" + longitude + "," + latitude + "?units=si";
-	request({url: darkskyUrl, json: true}, (error, {body, statusCode}) => {
-		if(error) {
-			callback(`Could not connect with the weather forecast dark-sky API service!`, undefined);
-		} else if(statusCode != 200) {
-			callback(`Error occurred, error code: ${statusCode} Error: ${error}`);
-		} else if(typeof body.currently === "undefined") {
-			callback(`Error on weather forecast data!`);
-		} else {
-			const data = body;
-			callback(undefined, {
-				temperature: data.currently.temperature,
-				rainPerc: data.currently.precipProbability
-			});
+	const darkskyUrl = "https://api.darksky.net/forecast/" + process.env.DARKSKY_API_TOKEN + "/" + latitude + "," + longitude + "?units=si";
+	(async () => {
+		try {
+			const darkskyRes = await superagent.get(darkskyUrl);
+			const tempRes = darkskyRes.body.currently;
+		} catch (err) {
+		  	console.error(err);
 		}
-	});
+	})();
 });
 
 
--- src/routers/geolocation.router.js
+++ src/routers/geolocation.router.js
@@ -1,28 +1,24 @@
 const express = require("express");
-const request = require("request");
+const superagent = require('superagent');
 
 const router = new express.Router();
 
-router.get('/geolocation', (req, res) => {
-    const address = req.body.address;
-    const mapboxUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + address + ".json?access_token=" + process.env.MAPBOX_API_TOKEN;
+router.get('/geolocation/:address', (req, res) => {
+    const address = req.params.address;
 	
-	request({url: mapboxUrl, json: true}, (error, {body, statusCode}) => {
-		if(error) {
-			callback("Unable to connect to service!", undefined);
-		} else if(body.features.length === 0) {
-			callback("Unable to find location! Try another location...", undefined);
-		} else if(statusCode != 200) {
-			callback("Something went wrong! " + error, undefined);
-		} else {
-			const data = body;
-			callback(undefined, {
-				longitude: data.features[0].center[1],
-				latitude: data.features[0].center[0],
-				location: data.features[0].place_name
-			});
+	const mapboxUrl = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + address + ".json?access_token=" + process.env.MAPBOX_API_TOKEN;
+	console.log(mapboxUrl);
+	(async () => {
+		try {
+			const mapboxRes = await superagent.get(mapboxUrl);
+
+			const longitude = mapboxRes.features[0].center[1];
+			const latitude = mapboxRes.features[0].center[0];
+			const location = mapboxRes.features[0].place_name;		
+		} catch (err) {
+		  	console.error(err);
 		}
-	});	
+	})();
 });
 
 module.exports = router;
